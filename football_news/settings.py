23d1e4adca1a654e463f3dc3314a4ed2258765d7
kalistawiarta <kalistawiarta13@gmail.com>
2025-09-10 20:38:41 +0700

    memperbaiki error

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", 'kalista-wiarta-footballnews.pbp.cs.ui.ac.id']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://kalista-wiarta-footballnews.pbp.cs.ui.ac.id/"
+    "https://kalista-wiarta-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
